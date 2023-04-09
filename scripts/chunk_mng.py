ca3c2ac4e63b3ad6085b86d17101f5b67e3b2333
Moritz <96340080+mol42069@users.noreply.github.com>
2023-04-09 12:21:14 +0200

    Update chunk_mng.py

--- scripts/chunk_mng.py
+++ scripts/chunk_mng.py
@@ -1,7 +1,15 @@
 
-def manage(cur_pos_x, chunks):
-    for chunk in chunks:
-        if chunk.x > 1800 or chunk.x < -1800:
-            chunk.loaded = False
-        else:
-            chunk.loaded = True
+def manage(cur_pos_x, chunks, chunk_width): 
+
+cur_chunk_pos = cur_pos_x / chunk_width # must be rounded up
+If chunks[0].x > cur_chunk_pos:
+    #gen chunks to the left
+elif chunks[-1] < cur_chunk_pos:
+    #gen chunks to the right 
+
+
+cur_chunks = [chunks[cur_chunk_pos - 1], chunks[cur_chunk_pos], chunks[cur_chunk_pos + 1]] 
+
+return cur_chunks
+
+
