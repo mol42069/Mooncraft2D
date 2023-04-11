e29fb9f073a3bdb3f31875cc4f507ca0e33a9b6c
Moritz <96340080+mol42069@users.noreply.github.com>
2023-04-11 07:50:11 +0200

    Update chunk_mng.py

--- scripts/chunk_mng.py
+++ scripts/chunk_mng.py
@@ -1,11 +1,20 @@
+import math
+from scripts import chunk
 
 def manage(cur_pos_x, chunks, chunk_width): 
 
-cur_chunk_pos = cur_pos_x / chunk_width # must be rounded up
+cur_chunk_pos = math.floor(cur_pos_x / chunk_width) # must be rounded down
 If chunks[0].x > cur_chunk_pos:
-    #gen chunks to the left
+    x = chunks[0].x - chunk_width
+    n_sprites = [chunks[0].sprites, chunks[0].s_atlas]
+    n_chunk = Chunk(x, chunks[0].y, chunk_width, chunks[0].seed, n_sprites, chunks[0].sprite_size)
+    chunks.insert(0, n_chunk)
+    cur_chunk_pos += 1
 elif chunks[-1] < cur_chunk_pos:
-    #gen chunks to the right 
+    x = chunks[-1].x + chunk_width
+    n_sprites = [chunks[0].sprites, chunks[0].s_atlas]
+    n_chunk = Chunk(x, chunks[0].y, chunk_width, chunks[0].seed, n_sprites, chunks[0].sprite_size)
+    chunks.append(n_chunk)
 
 
 cur_chunks = [chunks[cur_chunk_pos - 1], chunks[cur_chunk_pos], chunks[cur_chunk_pos + 1]] 
