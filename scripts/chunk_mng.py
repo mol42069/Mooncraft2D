40d537c47acb08d4eb04e39c238e08dcbb1792f1
Moritz <96340080+mol42069@users.noreply.github.com>
2023-04-11 09:06:18 +0200

    Add files via upload

--- scripts/chunk_mng.py
+++ scripts/chunk_mng.py
@@ -10,8 +10,9 @@ If chunks[0].x > cur_chunk_pos:
     n_chunk = Chunk(x, chunks[0].y, chunk_width, chunks[0].seed, n_sprites, chunks[0].sprite_size)
     chunks.insert(0, n_chunk)
     cur_chunk_pos += 1
+    
 elif chunks[-1] < cur_chunk_pos:
-    x = chunks[-1].x + chunk_width
+    x = chunks[-1].x + chunk_width
     n_sprites = [chunks[0].sprites, chunks[0].s_atlas]
     n_chunk = Chunk(x, chunks[0].y, chunk_width, chunks[0].seed, n_sprites, chunks[0].sprite_size)
     chunks.append(n_chunk)
